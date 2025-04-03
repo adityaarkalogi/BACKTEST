e1ffd490cfc11fb5c3b78977da686522cf56f00b
adityaarkalogi <adityaarkalogitech@gmail.com>
2025-04-03 16:11:01 +0530

    Removed files

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-file_path = os.path.join(os.getcwd(), 'BANKNIFTY_JF_FNO_11042023.feather')
-df = pd.read_feather(file_path)
-file_path = file_path.replace('.feather','.csv')
-df.to_csv(file_path)
-
